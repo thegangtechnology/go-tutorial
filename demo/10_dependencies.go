c0cb9026903402df8c0c0a2e99c72081a4b32c3b
Piti Ongmongkolkul <piti118@gmail.com>
2024-07-05 12:30:24 +0700

    add dependencies exercise

---
+++ demo/10_dependencies.go
@@ -0,0 +1,9 @@
+package demo
+
+//Lets use
+// go get github.com/fatih/color
+//func ColorfulHello() {
+//	color.Cyan("Prints text in cyan.")
+//	color.Blue("Prints %s in blue.", "text")
+//	color.Red("We have red")
+//}
--- exercise/2_read_the_doc.go
+++ exercise/2_read_the_doc.go
@@ -1,8 +1,12 @@
 package exercise
 
-// take home homework
+// Take home homework (should take 30 min max)
 // using net/http build an add number api no echo/gin/etc just plain net/http
 // chatgpt is your friend
 // don't worry about validation and so on we will do it in the next section.
+// This way is a bit too prone to error.
+// The point here is for you to be able to read go doc.
+// It's like javadoc but worse....
+// https://pkg.go.dev/net/http
 
 // POST localhost/add {"a":1, "b":2} ==> {"result": 3}
